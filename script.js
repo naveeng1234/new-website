6a17239a8c771640f224d3b42944c6dc4cfaed5e
naveeng1234 <naveenshephard07@gamil.com>
2025-03-06 20:18:36 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -51,3 +51,31 @@ function setLightMode() {
     icon.src = icon.getAttribute("src-light");
   });
 }
+const textElement = document.getElementById("typing-text");
+const words = ["DevOps Engineer", "Cloud Architect", "Automation Expert"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+    const currentWord = words[wordIndex];
+    if (isDeleting) {
+        charIndex--;
+    } else {
+        charIndex++;
+    }
+
+    textElement.innerText = currentWord.substring(0, charIndex);
+
+    if (!isDeleting && charIndex === currentWord.length) {
+        setTimeout(() => (isDeleting = true), 1000);
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+    }
+
+    setTimeout(typeEffect, isDeleting ? 100 : 200);
+}
+
+typeEffect();
+
